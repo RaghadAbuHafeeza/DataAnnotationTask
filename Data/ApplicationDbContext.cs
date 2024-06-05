5fca795b37a9f4d19e050337a23333cea8bb9b0c
RaghadAbuHafeeza <raghadabuhafeeza@gmail.com>
2024-06-06 01:51:53 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataAnnotationTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnnotationTask.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\NEWSQLSERVER; DataBase=dataAnnotation; Trusted_Connection=true; TrustServerCertificate=true;");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using DataAnnotationTask.Data;
+using DataAnnotationTask.Models;
+using System;
+using System.Linq;
+
+namespace DataAnnotationTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            Department department = new Department();
+
+            Console.WriteLine("Name Is: ");
+            department.Name = Console.ReadLine();
+
+            context.Departments.Add(department);
+
+            context.SaveChanges();
+
+        }
+    }
+}
